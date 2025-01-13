a3ccc9b82eebafb5dc0e39bf021be68b4edd80f2
Dmytro Voloshyn <dmytrovoloshyn@Mac.lan>
2025-01-13 12:47:09 -0700

    clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import RaMContainer from './ram-container/RaMContainer'
-import RaMPage from './ram-container/ram-page/RaMPage'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import RaMContainer from "./components/ram-container/RaMContainer";
+import RaMPage from "./components/ram-container/ram-page/RaMPage";
 
 function App() {
-  
   return (
-    <> 
+    <>
       <Routes>
-          <Route path="/" element={<RaMContainer/>} />
-          <Route path="/:id" element={<RaMPage />} />
+        <Route path="/" element={<RaMContainer />} />
+        <Route path="/:id" element={<RaMPage />} />
       </Routes>
     </>
-   
-       
-  )
+  );
 }
 
-export default App
+export default App;
